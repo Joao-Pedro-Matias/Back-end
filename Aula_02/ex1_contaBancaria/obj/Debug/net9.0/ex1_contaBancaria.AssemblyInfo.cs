65cc7b709d52f3ec65245fb745fdf03583eeba94
Joao_Pedro_Matias <joaopedromatias2007@gmail.com>
2025-11-13 22:48:32 -0300

    Aula03_API

--- Aula_02/ex1_contaBancaria/obj/Debug/net9.0/ex1_contaBancaria.AssemblyInfo.cs
+++ Aula_02/ex1_contaBancaria/obj/Debug/net9.0/ex1_contaBancaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex1_contaBancaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8215aee4eed43093b55b87cdaa37fd68a154b7bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8d3791667a34821d9a0de3cc6deb72dba938e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex1_contaBancaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex1_contaBancaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
